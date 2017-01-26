01da428da5acf1c77100a00b5595b7bb5e12d8e5
K2898 <K2898@OHJE-330-WS-27>
2017-01-26 11:07:04 +0200

    oooleeoliolioiee

---
+++ CarApplication/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class CarApplication
+    {
+        static void Main(string[] args)
+        {
+            //create one instance of car class
+            Car car = new Car();
+            car.PrintData();
+            car.Model = "Datsun 100A";
+            car.Color = "Yellow";
+            car.Engine = 1.0;
+            car.Speed = 10;
+            car.FuzzyDices = true;
+            car.DoorCount = 2;
+            car.PrintData();
+        }
+    }
+}
---
+++ CarApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Car
+    {
+        //properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool FuzzyDices { get; set; }
+        public int DoorCount { get; set; }
+        public static int SomeProperty { get; set; }
+        
+        //constructor
+
+        //method to give more speed
+
+        public void Accelerate(int value)
+        {
+            Speed += value;
+        }
+        public void Brake(int value)
+        {
+            Speed -= value;
+        }
+
+        //method to display car properties
+        public void PrintData()
+        {
+            Console.WriteLine("Car data:");
+            Console.WriteLine("- model:" + Model);
+            Console.WriteLine("- color:" + Color);
+            Console.WriteLine("- engine:" + Engine);
+            Console.WriteLine("- speed:" + Speed);
+            Console.WriteLine("- dices" + FuzzyDices);
+            Console.WriteLine("- doors:" + DoorCount);
+            Console.WriteLine("- some static property:" + SomeProperty);
+
+        }
+        //destructor
+        ~Car()
+        {
+            Console.WriteLine("Car Object destroyed!");
+        }
+        
+    }
+}
