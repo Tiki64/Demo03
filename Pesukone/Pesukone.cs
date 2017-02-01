c23d02c5b05bbfc932078e779a1a1c682d430eba
Tiki <Tiki@DESKTOP-7SCSCAC>
2017-02-01 19:06:28 +0200

    Valmis t2

---
+++ Pesukone/Pesukone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesukoneApplication
+{
+    class Pesukone
+    {
+        //properties
+        public string Model { get; set; }
+        public int Temperature { get; set; }
+        public bool On { get; set; }
+
+        //constructors
+       
+        public Pesukone(int _temp)
+        {
+            Temperature = _temp;
+        }
+
+        //methods
+        public void PrintData()
+        {
+            Console.WriteLine("Pesukoneen tiedot:");
+            Console.WriteLine("- Model:" + Model);
+            Console.WriteLine("- Temperature:" + Temperature);
+            Console.WriteLine("- ON " + On);
+            Console.WriteLine(">");
+        }
+        //destructor
+        ~Pesukone()
+        {
+            Console.WriteLine("Pesukone Object destroyed!");
+        }
+
+    }
+}
--- Pesukone/Program.cs
+++ Pesukone/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pesukone
+namespace PesukoneApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //create one instance of class
+            Pesukone pesukone = new Pesukone(40);
+            pesukone.Model = "Pesukone A1";
+            pesukone.On = true;
+            pesukone.PrintData();
         }
     }
 }
